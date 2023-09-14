aaba608e9e9fbaf31bf6e885fe8c8ac63b568e42
Omar Akbar <fa20-bce-077@cuilahore.edu.pk>
2023-09-14 17:05:59 +0500

    Update cgpaplot.py
    lab task

--- cgpaplot.py
+++ cgpaplot.py
@@ -1,4 +1,12 @@
-import matplotlib.pyplot as plt
-plt.plot([3.5,3.1,3.0,2.9,2.5,2.3,2.0,2.0])
-plt.ylabel("My CGPA")
-plt.show()
+current_cgpa = float(input("Enter your current CGPA: "))
+total_credit_hours_studied = int(input("Enter the total credit hours studied: "))
+
+current_semester_credit_hours = int(input("Enter current semester credit hours: "))
+future_cgpa = float(input("Enter your desired future CGPA: "))
+
+
+total_points = current_cgpa * total_credit_hours_studied
+
+cpga = total_points / total_credit_hours_studied
+
+print(cpga)
